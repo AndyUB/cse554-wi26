38448ac0e09d133dd318e555f18ac87ccb82da0e
Andy Ruan <yhruan22@cs.washington.edu>
2026-01-27 14:54:06 -0800

    RMS norm vector
    Signed-off-by: Andy Ruan <yhruan22@cs.washington.edu>

---
+++ assignment1/rms_norm/vector/CMakeFiles/rms_norm_vector.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/local1/groups/554g13/cse554-winter-2026/assignment1/rms_norm/vector/main.cu" "CMakeFiles/rms_norm_vector.dir/main.cu.o" "gcc" "CMakeFiles/rms_norm_vector.dir/main.cu.o.d"
+  "/local1/groups/554g13/cse554-winter-2026/assignment1/rms_norm/vector/rms_norm_vector.cu" "CMakeFiles/rms_norm_vector.dir/rms_norm_vector.cu.o" "gcc" "CMakeFiles/rms_norm_vector.dir/rms_norm_vector.cu.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
